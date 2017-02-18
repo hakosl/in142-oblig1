006c3a0db9fc8f1d3d71c642f4f73d0df30e79dc
Håkon Osland <hakon.ettestol.osland@gmail.com>
2017-02-18 18:54:23 +0100

    added a bunch of useless features

--- src/client.java
+++ src/client.java
@@ -5,40 +5,64 @@ import javax.xml.crypto.Data;
 import java.io.*;
 import java.net.*;
 import java.util.Scanner;
+import java.util.stream.Stream;
 
 public class client {
     private static int port = 6789;
     private static String ip = "localhost";
     private static Scanner sc = new Scanner(System.in);
+    private static Socket clientSocket;
+    private static BufferedReader inFromServer;
+
+    public static void printSocket(Iterable<String> it) throws Exception{
+        for(String line : it) {
+            if(line.equals("END")) {return;}
+            if(line.equals("CLOSE")) {
+                clientSocket.close();
+                System.exit(0);
+            }
+            System.out.println("Server response: " + line);
+        }
+    }
+
+    public static void readlines() throws Exception{
+        Stream<String> lines = inFromServer.lines();
+
+        printSocket(lines::iterator);
+    }
 
     public static void main(String[] args) throws Exception{
-        System.out.println("Enter the server ip: ");
-        ip = sc.nextLine();
+        /*System.out.println("Enter the server ip: ");
+        ip = sc.nextLine();*/
 
 
-        Socket clientSocket = new Socket(ip, port);
+        clientSocket = new Socket(ip, port);
 
         DataOutputStream outToServer = new DataOutputStream(clientSocket.getOutputStream());
 
-        BufferedReader inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        inFromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
 
 
         String responseString;
         String message;
 
         System.out.println("Connection established with: " + clientSocket.getRemoteSocketAddress());
+        readlines();
 
-        do {
-            System.out.println("enter your command: ");
-            message = sc.nextLine();
 
-            outToServer.writeBytes(message + "\n");
-            outToServer.flush();
+        commandLoop:
+        {
+            do {
+                System.out.println("enter your command: ");
+                message = sc.nextLine();
 
-            responseString = inFromServer.readLine();
-            System.out.println("the server responded with: " + responseString);
+                outToServer.writeBytes(message + "\n");
+                outToServer.flush();
+                readlines();
+            /*responseString = inFromServer.readLine();
+            System.out.println("the server responded with: " + responseString);*/
 
-        } while (!responseString.equals("CLOSE"));
-        clientSocket.close();
+            } while (true);
+        }
     }
 }
--- src/server.java
+++ src/server.java
@@ -9,8 +9,6 @@ public class server {
     private static int port = 6789;
     public static void main(String[] args) throws Exception {
 
-        System.out.println(Arrays.toString(args));
-
         ServerSocket socket = new ServerSocket(port);
 
         while(true) {
